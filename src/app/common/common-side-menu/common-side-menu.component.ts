ec61967fbe9b2b3184f09992e07a4c39b9b1619c
Ramesh <mramesh.chinna@gmail.com>
2022-02-13 04:04:32 +0530

    updated

--- src/app/common/common-side-menu/common-side-menu.component.ts
+++ src/app/common/common-side-menu/common-side-menu.component.ts
@@ -31,7 +31,9 @@ export class CommonSideMenuComponent{
     this.loginId=this.route.snapshot.params.id
     this.loginId=localStorage.getItem('loginId')
     this.emailId=localStorage.getItem('emailId')
-    
+    if(!this.emailId && !this.loginId){
+      window.location.reload();
+    }
     //console.log("🚀 ~ file: common-side-menu.component.ts ~ line 27 ~ CommonSideMenuComponent ~  this.loginId",  this.loginId)
   }
   Logout(){
